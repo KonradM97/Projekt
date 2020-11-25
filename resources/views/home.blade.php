d83aab50e0860b31ca18fa8f623650f7ac491cb6
fazi230 <konrad.fazi230@gmail.com>
2020-11-25 01:17:12 +0100

    spójność stylów w profile i panel(home)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <link href="css/homebladestyle.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-<div class="container">
+<div class="content">
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="row">
